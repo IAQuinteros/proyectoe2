00acdd9a0534bbc8be2f17ce2bfa5088fbc68629
Ignacio Quinteros <iaquinteros1@miuandes.cl>
2025-05-14 16:08:59 -0400

    ajustes

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,5 +1,6 @@
 class Follower < ApplicationRecord
   belongs_to :user, class_name: "User"
+  belongs_to :user2, class_name: 'User', foreign_key: 'user2'
 
   validates :date_followers, presence: true
   validates :user2, presence: true 
